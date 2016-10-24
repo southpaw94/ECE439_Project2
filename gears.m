7401f975d9a47954fcfd79b68eb02ea4a08db27f
Michael Estes <mjestes1994@gmail.com>
2016-10-24 17:27:31 -0400

    Updated with sequential image processing for both images (Part 1)

---
+++ gears.m
@@ -0,0 +1,28 @@
+gears0 = imread('GearsAndWashers2.png');
+gears_bin = edge(gears0, 'Sobel');
+
+imwrite(gears_bin, 'gears2_bin.png')
+
+strel_hor = strel('line', 3, 0);
+strel_ver = strel('line', 3, 90);
+gears_dil = imdilate(gears_bin, strel_hor);
+gears_dil = imdilate(gears_dil, strel_ver);
+gears_dil = imfill(gears_dil, 'holes');
+
+imwrite(gears_dil, 'gears2_dil.png')
+
+gears_lab = bwlabel(gears_dil);
+max_val = max(reshape(gears_lab, 1, []));
+gears_lab = double(gears_lab) / max_val * 255;
+
+vislabels(gears_lab)
+
+%imshow(gears_lab)
+%hold on
+boundaries = bwboundaries(gears_lab);
+
+for i = 1:length(boundaries)
+    boundary = boundaries{i};
+%    plot(boundary(:, 2), boundary(:, 1), 'r', ...
+%    'LineWidth', 2)
+end
